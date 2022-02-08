e8f56508336d8f8ba6bb0efa937d2b372c50d505
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2022-02-08 09:27:12 +0100

    Updated with localstorage

--- angular-communication/src/app/components/todos/todos.component.ts
+++ angular-communication/src/app/components/todos/todos.component.ts
@@ -7,16 +7,20 @@ import { Todo } from 'src/app/models/Todo';
   styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent implements OnInit {
-  // Definiera vår lista och skapa upp två stycken test-objekt
-  todos: Todo[] = [new Todo('Handla', 1), new Todo('Träna', 2)];
+  // Definiera vår lista.
+  todos: Todo[] = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    let todosString: string = localStorage.getItem('todos') || '[]';
+    this.todos = JSON.parse(todosString);
+  }
 
   // Funktion som körs när händelsen från addTodo-komponenten inträffar.
   // Kommer att lägga till ett nytt objekt i listan todos.
   create(todoText: string) {
     this.todos.push(new Todo(todoText, 10));
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
